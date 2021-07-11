dba84544420d5f6193b8fce1974e971368991744
reyhannaufal <reyhannaufal21@gmail.com>
2021-07-11 21:24:26 +0800

    feat: add footer and form section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./index.html'],
+  purge: ["./index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -7,5 +7,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
